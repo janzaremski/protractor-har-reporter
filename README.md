1d5f491bf4c69b7267c895ae524ded392ffd185f
Jan.Zaremski <jan.zaremski@finastra.com>
2020-10-27 16:21:52 +0100

    HARReporter init

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # protractor-har-reporter
+
+Protractor reporter that is capturing [HAR files](https://en.wikipedia.org/wiki/HAR_(file_format)) from browser network traffic and saves them for later inspection.
+
+## Install
+
+```
+npm i --save protractor-har-reporter
+```
+
+## Usage
+
+In your Protractor configuration file:
+
+```js
+import {HARReporter} from 'protractor-har-reporter'
+
+...
+
+onPrepare: () => {
+
+    const harReporter = new HARReporter({
+        resultsDir: './results'
+    });
+
+    jasmine.getEnv().addReporter(harReporter);
+}
+```
+
+HAR files will be saved in the `harfiles` subdir of `resultsDir`
---
+++ src/har-reporter.ts
@@ -0,0 +1,76 @@
+import { browser } from 'protractor';
+import { harFromMessages } from 'chrome-har';
+import * as fse from 'fs-extra';
+import * as path from 'path';
+
+interface Configuration {
+    resultsDir: string
+}
+
+export class HARReporter {
+
+    resultsDir: string;
+
+    constructor(configuration: Configuration) {
+        this.resultsDir=configuration.resultsDir;   
+    }
+
+    _asyncFlow: Promise<any>;
+
+    jasmineStarted() {
+        /* Wait for async tasks triggered by `suiteDone`. */
+        afterAll(async () => {
+            await this._asyncFlow;
+            this._asyncFlow = null;
+        });
+    }
+
+    suiteDone(result) {
+        this._asyncFlow = this._asyncSuiteDone(result);
+    }
+
+    async _asyncSuiteDone(result) {
+        const browserLogs = await browser.manage().logs().get('performance');
+        if (browserLogs.length > 0) {
+            const specName = result.fullName.replace(/\s+/g, '-').toLowerCase();
+            const harDir = path.resolve(this.resultsDir, 'harfiles');
+            await fse.ensureDir(harDir)
+            const harPath = path.resolve(harDir, `${specName}.har`);
+            const events = [];
+            const addResponseBodyPromises = [];
+            browserLogs.forEach(browserLog => {
+                const message = JSON.parse(browserLog.message);
+                const harEvent = {
+                    method: message.message.method,
+                    params: message.message.params
+                };
+                events.push(harEvent);
+                if (harEvent.method === 'Network.responseReceived') {
+                    const response = harEvent.params.response;
+                    const requestId = harEvent.params.requestId;
+                    // Response body is unavailable for redirects, no-content, image, audio and video responses
+                    if (response.status !== 204 &&
+                        response.headers.location == null &&
+                        !response.mimeType.includes('image') &&
+                        !response.mimeType.includes('audio') &&
+                        !response.mimeType.includes('video')
+                    ) {
+                        const addResponseBodyPromise = browser.driver.sendChromiumCommandAndGetResult('Network.getResponseBody', { requestId }).then(responseBody => {
+                            // Set the response so chrome-har can add it to the HAR file
+                            harEvent.params.response = {
+                                ...response,
+                                //@ts-ignore
+                                body: Buffer.from(responseBody.body, responseBody.base64Encoded ? 'base64' : undefined).toString()
+                            };
+                        }, (reason) => { });
+                        addResponseBodyPromises.push(addResponseBodyPromise);
+                    }
+                }
+            });
+            await Promise.all(addResponseBodyPromises);
+            const harObject = harFromMessages(events, { includeTextFromResponseBody: true });
+            fse.writeFileSync(harPath, JSON.stringify(harObject));
+            console.log(`Saving HAR file to ${harPath}`);
+        }
+    }
+}
